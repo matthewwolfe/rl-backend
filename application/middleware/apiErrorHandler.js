a1d0fa84ffc1a9efe620dd356172a8de8ae4e15c
matthewwolfe <matthew.wolfe2@gmail.com>
2018-05-11 17:40:11 -0400

    fix always throw

--- application/middleware/apiErrorHandler.js
+++ application/middleware/apiErrorHandler.js
@@ -6,9 +6,10 @@ function apiErrorHandler(error, request, response, next) {
     if (error instanceof ValidationError || error instanceof HttpError) {
         response.status(error.status).send(error);
     }
-
-    throw error;
-    response.status(500).send('Something went wrong.');
+    else {
+        throw error;
+        response.status(500).send('Something went wrong.');
+    }
 }
 
 module.exports = apiErrorHandler;
